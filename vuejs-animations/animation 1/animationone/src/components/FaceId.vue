3f18e6e083dfd6723e3e169139de1c7526395adf
Daniel Albino <daniel.albino@polygon.pt>
2019-09-19 12:47:03 +0100

    added node.js exercises

---
+++ vuejs-animations/animation 1/animationone/src/components/FaceId.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+      <g class="image">
+        <rect fill="#dbdbdb" height="464" rx="48" width="272" x="120" y="24" />
+        <rect fill="#2dc2b4" height="432" rx="48" width="272" x="120" y="24" />
+        <path d="m360 264h-208v-56a104 104 0 0 1 104-104 104 104 0 0 1 104 104z" fill="#8e7760" />
+        <path d="m184 296h-16a24 24 0 0 1 -24-24 24 24 0 0 1 24-24h8z" fill="#efd6ae" />
+        <path d="m344 296h-16l8-48h8a24 24 0 0 1 24 24 24 24 0 0 1 -24 24z" fill="#efd6ae" />
+        <g fill="#2ed6d1">
+          <path d="m160 128h-16v-24a8 8 0 0 1 8-8h24v16h-16z" />
+          <path d="m368 128h-16v-16h-16v-16h24a8 8 0 0 1 8 8z" />
+          <path d="m176 408h-24a8 8 0 0 1 -8-8v-24h16v16h16z" />
+          <path d="m360 408h-24v-16h16v-16h16v24a8 8 0 0 1 -8 8z" />
+        </g>
+        <path
+          d="m294.018 370.879a78.285 78.285 0 0 1 -76.036 0 78.286 78.286 0 0 1 -39.48-57.362 249.008 249.008 0 0 1 -2.5-35.212v-33.035a91.336 91.336 0 0 1 17.013-53.088l22.985-32.182a95.494 95.494 0 0 0 77.481 52.457l42.519 3.543v62.305a249.008 249.008 0 0 1 -2.5 35.212 78.286 78.286 0 0 1 -39.482 57.362z"
+          fill="#efd6ae"
+        />
+        <circle cx="216" cy="256" fill="#9b8268" r="16" />
+        <circle cx="296.003" cy="256" fill="#9b8268" r="16" />
+        <path
+          d="m255.992 312a8.01 8.01 0 0 1 -3.57-.844l-16-8a8 8 0 0 1 -3.578-10.734l16-32a8 8 0 0 1 14.312 7.156l-12.422 24.844 8.844 4.422a8 8 0 0 1 -3.586 15.156z"
+          fill="#9b8268"
+        />
+        <path
+          d="m256 343.289a51.2 51.2 0 0 1 -28.438-8.633 8 8 0 1 1 8.876-13.312 35.174 35.174 0 0 0 39.124 0 8 8 0 0 1 8.876 13.312 51.2 51.2 0 0 1 -28.438 8.633z"
+          fill="#9b8268"
+        />
+        <rect fill="#2ab3a6" height="32" rx="16" width="128" x="192" y="24" />
+        <path class="scan" d="m120 304h272v16h-272z" />
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.scan {
+  fill: none;
+}
+
+@keyframes scanning {
+  0%,
+  100% {
+    fill: #ff496c;
+    opacity: 0.6;
+    transform: translate(0px, 90px);
+  }
+  50% {
+    fill: #ff496c;
+    opacity: 0.6;
+    transform: translate(0px, -210px);
+  }
+}
+
+.image .scan {
+  animation: scanning 4s ease-in-out infinite;
+}
+</style>
+
---
+++ vuejs-animations/animation 1/animationone/src/views/Home.vue
@@ -0,0 +1,422 @@
+<template>
+  <div class="container row col-12">
+    <div id="top-animation" class="col-12">
+      <div class="col-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100"
+          height="100"
+          viewBox="0 0 28.699 31.999"
+        >
+          <g>
+            <path
+              class="a"
+              d="M357.223,49.041,345.51,42.347a2.637,2.637,0,0,0-2.616,0L331.18,49.041a2.637,2.637,0,0,0-1.328,2.289v13.34a2.637,2.637,0,0,0,1.328,2.289l11.713,6.693a2.637,2.637,0,0,0,2.616,0l11.713-6.693a2.637,2.637,0,0,0,1.328-2.289V51.33A2.637,2.637,0,0,0,357.223,49.041Zm-3.455,14.425L344.2,68.933l-9.566-5.467V52.533l9.566-5.467,9.566,5.467Z"
+              transform="translate(-329.852 -42)"
+            />
+            <path
+              class="b"
+              d="M382.574,91.513l-7.175,4.1v8.2l7.175,4.1,7.175-4.1v-8.2Zm4.783,10.933-4.783,2.733-4.783-2.733V96.98l4.783-2.733,4.783,2.733Z"
+              transform="translate(-368.224 -83.713)"
+            />
+            <path
+              class="c"
+              d="M433.966,49.041l-11.713-6.693A2.635,2.635,0,0,0,420.945,42v5.066h0l9.566,5.467V63.466l-9.566,5.467h0V74a2.635,2.635,0,0,0,1.308-.347l11.713-6.693a2.637,2.637,0,0,0,1.328-2.289V51.33A2.637,2.637,0,0,0,433.966,49.041Z"
+              transform="translate(-406.595 -42)"
+            />
+            <path
+              class="d"
+              d="M420.945,91.513h0v2.733h0l4.783,2.733v5.467l-4.783,2.733h0v2.733h0l7.175-4.1v-8.2Z"
+              transform="translate(-406.595 -83.713)"
+            />
+          </g>
+        </svg>
+        <h1 class="text" style="color:white;">Polygon</h1>
+        <p class="paragraph">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sapien
+          pellentesque habitant morbi tristique senectus et. Dui sapien eget mi
+          proin.""
+        </p>
+      </div>
+    </div>
+    <div class="middle row col-12">
+      <!-- Card -->
+      <div class="col-4">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src="../../public/img/biometridon.svg" alt="Avatar" />
+              <h4>OnBoarding</h4>
+            </div>
+            <div class="flip-card-back">
+              <p>“Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- / Card -->
+
+      <!-- Card -->
+      <div class="col-4">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src="../../public/img/fingerprint.svg" alt="Avatar" />
+              <h4>Authentication</h4>
+            </div>
+            <div class="flip-card-back">
+              <p>“Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- / Card -->
+
+      <!-- Card -->
+      <div class="col-4">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src="../../public/img/technical-support.svg" alt="Avatar" />
+              <h4>Us as a Service</h4>
+            </div>
+            <div class="flip-card-back">
+              <p>“Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- / Card -->
+    <div class="col-12">
+      <hr />
+    </div>
+    <div class="row col-12">
+      <div class="col-6">
+        <faceid></faceid>
+      </div>
+      <div class="col-6">
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-5">Face ID</h1>
+            <p
+              class="lead"
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam facere nostrum assumenda culpa saepe fugiat qui magnam exercitationem, eveniet odio nesciunt iste atque sed est consectetur ut nulla maiores error?</p>
+          </div>
+          <button class="btn btn-primary">See more</button>
+        </div>
+      </div>
+    </div>
+    <!-- B-BANK -->
+    <div class="b-bank row col-12">
+      <div class="col-4">
+        <img src="../../public/img/BiomtriD_logo blue.svg" />
+        <h1>B-Bank</h1>
+      </div>
+      <div class="col-6">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Illo minus amet atque impedit minima,
+        nulla culpa optio recusandae earum nam ab ipsum tempore odit neque natus dicta dolorem?
+        Mollitia, eius.
+      </div>
+    </div>
+    <!-- / B-BANK -->
+    <!-- B-BANK FETURES -->
+    <div class="features col-12 row">
+      <!-- FEATURE -->
+      <div class="col-3">
+        <div class="row col-12" style="display: inline;">
+          <img src="../../public/img/programming.svg" alt style="width: 50px; height: 50px;" />
+          <h5>Feature one</h5>
+          <div class="col-12">
+            <p>Far far away, behind the word mountains, far from the countries</p>
+          </div>
+        </div>
+      </div>
+      <!-- / FEATURE -->
+      <!-- FEATURE -->
+      <div class="col-3">
+        <div class="row col-12" style="display: inline;">
+          <img src="../../public/img/programming.svg" alt style="width: 50px; height: 50px;" />
+          <h5>Feature Two</h5>
+          <div class="col-12">
+            <p>Far far away, behind the word mountains, far from the countries</p>
+          </div>
+        </div>
+      </div>
+      <!-- / FEATURE -->
+
+      <!-- FEATURE -->
+      <div class="col-3">
+        <div class="row col-12" style="display: inline;">
+          <img src="../../public/img/programming.svg" alt style="width: 50px; height: 50px;" />
+          <h5>Feature Three</h5>
+          <div class="col-12">
+            <p>Far far away, behind the word mountains, far from the countries</p>
+          </div>
+        </div>
+      </div>
+      <!-- / FEATURE -->
+
+      <!-- FEATURE -->
+      <div class="col-3">
+        <div class="row col-12" style="display: inline;">
+          <img src="../../public/img/programming.svg" alt style="width: 50px; height: 50px;" />
+          <h5>Feature Four</h5>
+          <div class="col-12">
+            <p>Far far away, behind the word mountains, far from the countries</p>
+          </div>
+        </div>
+      </div>
+      <!-- / FEATURE -->
+    </div>
+    <!-- / B-BANK FETURES -->
+
+    <div class="col-12">
+      <hr />
+    </div>
+
+    <!-- Card -->
+    <div class="col-12 row solution">
+      <div class="col-4">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src="../../public/img/locked.svg" />
+              <p style="font-size: 14px;">Identification, Authentication and Authorization</p>
+            </div>
+            <div class="flip-card-back">
+              <p>Para login/acesso, consultas e transações com deep linking, com superior experiência de utilização.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- / Card -->
+
+      <!-- Card -->
+      <div class="col-4">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src="../../public/img/accounting.svg" />
+              <p style="font-size: 14px;">Remote account opening</p>
+            </div>
+            <div class="flip-card-back">
+              <p>Para abrir novas contas bancárias remotamente ou para contratação de outros produtos financeiros, através de canais digitais.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- / Card -->
+
+      <!-- Card -->
+      <div class="col-4">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src="../../public/img/face.svg" />
+              <p style="font-size: 14px;">Reliable dentification of the client</p>
+            </div>
+            <div class="flip-card-back">
+              <p>Inteligência sobre experiência em loja e personalização do atendimento</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- / Card -->
+    </div>
+    <div class="col-12">
+      <h1></h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import FaceID from "../components/FaceId";
+
+export default {
+  name: "home",
+  data() {
+    return {
+      flip: false
+    };
+  },
+  components: {
+    faceid: FaceID
+  },
+
+  methods: {
+    flipping() {
+      this.flip = !this.flip;
+      console.log(this.flip);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  padding: 0;
+  margin: 0;
+}
+
+#top-animation {
+  padding-top: 30px;
+  width: 100%;
+  height: 400px;
+  background: url("../../public/img/header_imag+ slogan.png");
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.solution {
+  margin: auto;
+  padding-left: 5%;
+  color: white;
+  padding-top: 30px;
+  width: 100%;
+  height: 200px;
+}
+
+.b-bank {
+  width: 100%;
+  padding-top: 5%;
+  height: 200px;
+  background: linear-gradient(to right, #cc33ff 0%, #0066cc 50%, #66ccff 100%);
+
+  color: white;
+}
+
+.features {
+  margin: auto;
+  padding-top: 3%;
+}
+
+/* ---------------------------------------- */
+.a {
+  fill: #ffffff;
+  animation: anime 2s ease-in-out;
+}
+
+.b {
+  fill: #00a7ba;
+  animation: anime 2s ease-in-out;
+}
+
+.c {
+  fill: #cfcccc;
+  animation: anime 2s ease-in-out;
+}
+
+.d {
+  fill: #006975;
+  animation: anime 2s ease-in-out;
+}
+
+/* ---------------------------------------- */
+.text {
+  animation: anime-text 3s ease-in-out;
+}
+
+.paragraph {
+  color: white;
+  justify-content: justify;
+  animation: anime-text 5s ease-in-out;
+}
+
+.middle {
+  margin: auto;
+  padding-left: 5%;
+  padding-top: 7%;
+  padding-bottom: 7%;
+}
+
+/* ---------------------------------------- */
+/* CARD FLIP  */
+/* ---------------------------------------- */
+.flip-card {
+  background-color: transparent;
+  width: 200px;
+  height: 150px;
+  perspective: 1000px;
+}
+
+.flip-card img {
+  padding-top: 10px;
+  margin: auto;
+  width: 100px;
+  height: 100px;
+}
+
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.6s;
+  transform-style: preserve-3d;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+  cursor: pointer;
+}
+
+.flip-card-front,
+.flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+}
+
+.flip-card-front {
+  background-color: white;
+  color: black;
+}
+
+.flip-card-back {
+  background-color: #2980b9;
+  color: white;
+  transform: rotateY(180deg);
+}
+
+.flip-card-back p {
+  padding-top: 25px;
+}
+
+/* ---------------------------------------- */
+.facescan {
+  border: black solid 2px;
+  padding-left: 0;
+}
+
+.jumbotron {
+  background-color: white;
+  border: rgb(211, 209, 209) solid 1px;
+}
+
+/* ---------------------------------------- */
+/* KEYFRAMES */
+/* ---------------------------------------- */
+@keyframes anime {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes anime-text {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
